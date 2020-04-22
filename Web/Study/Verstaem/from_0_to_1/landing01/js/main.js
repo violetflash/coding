b7528e2b6ee8a913656c5c84a2f995b721d003e2
Alexander Grobovsky <ktotaam@mail.ru>
2020-04-22 13:20:42 +0500

    22.04-home

--- Web/Study/Verstaem/from_0_to_1/landing01/js/main.js
+++ Web/Study/Verstaem/from_0_to_1/landing01/js/main.js
@@ -4,8 +4,10 @@ $(function () {
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
         infinite: false
+
     });
 
+    $('select').styler();
 
 
 
