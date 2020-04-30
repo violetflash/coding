2002ddc0d548cbba35eef601f5004e4a3945f20f
Alexander Grobovsky <ktotaam@mail.ru>
2020-05-01 01:10:48 +0500

    01.05-home

--- Web/Study/Verstaem/from_0_to_1/7h_landing01/js/main.js
+++ Web/Study/Verstaem/from_0_to_1/7h_landing01/js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
 
     $(".slider__inner, .news__slider-inner").slick({
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
@@ -8,7 +8,7 @@ $(function(){
 
     $('select').styler();
 
-    $('.header__btn-menu').on('click', function(){
+    $('.header__btn-menu').on('click', function () {
         $('.menu ul').slideToggle();
     });
 
--- Web/Study/Verstaem/from_0_to_1/to2rent/js/main.js
+++ Web/Study/Verstaem/from_0_to_1/to2rent/js/main.js
@@ -1,3 +1,17 @@
-$(function(){
+$(function () {
 
+    // - реинициализация слайдера, если сразу не заработал.
+    // $(".slider").slick('reinit');
+    $('.slider').slick({
+        prevArrow: false,
+        nextArrow: false,
+        infinite: true,
+        fade: true,
+        cssEase: 'linear',
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+
+    });
 });
+
