440b77b84d0fc79f1477b7839feffc9098ed031e
Alexander Grobovsky <ktotaam@mail.ru>
2020-05-08 12:52:18 +0500

    08.05-work

--- Web/Study/0_to_1-course/project_04.2-dop/js/main.js
+++ Web/Study/0_to_1-course/project_04.2-dop/js/main.js
@@ -1,8 +1,47 @@
-$(function () {
-    //initialize swiper when document ready
-    var mySwiper= new Swiper('.swiper-container', {
-        // Optional parameters
-        direction: 'vertical',
-        loop: true
-    })
+// $(function () {
+//     //initialize swiper when document ready
+//     var mySwiper= new Swiper('.swiper-container', {
+//         // Optional parameters
+//         autoHeight: true,
+//         direction: 'vertical',
+//         loop: true,
+//         slidesPerView: 1,
+//         pagination: {
+//             el: '.swiper-pagination',
+//             type: 'bullets',
+//         },
+//     })
+// });
+
+function resizeSwiper() {
+    $('.swiper-container').height($('.swiper-slide img').height())
+}
+$('.swiper-container').each(function(){
+    // Hero swiper
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction: 'horizontal',
+        loop: true,
+        effect: 'fade',
+        autoplay: {
+            delay: 3000,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        grabCursor: true,
+        keyboardControl: true,
+        speed: 1000,
+        slidesPerView:1,
+        onImagesReady: function (swiper) {
+            resizeSwiper();
+            swiper.onResize();
+        }
+    });
+});
+$(window).resize(function(){
+    resizeSwiper()
 });
