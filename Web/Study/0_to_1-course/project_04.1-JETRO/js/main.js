d9a4fd5266299921b092f04f8fc16856ed8c3e99
Alexander Grobovsky <ktotaam@mail.ru>
2020-05-12 12:56:57 +0500

    12.05-work

--- Web/Study/0_to_1-course/project_04.1-JETRO/js/main.js
+++ Web/Study/0_to_1-course/project_04.1-JETRO/js/main.js
@@ -3,17 +3,51 @@ $(function () {
     $('.slider-top__inner').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        asNavFor: '.slider-bottom__inner'
+        asNavFor: '.slider-bottom__inner',
+        adaptiveHeight: true,
     });
 
     $('.slider-bottom__inner').slick({
         slidesToShow: 6,
         slidesToScroll: 6,
         asNavFor: '.slider-top__inner',
-        variableWidth: true,
+        // variableWidth: true,
         focusOnSelect: true,
         infinite: false,
+        responsive: [
+            {
+                breakpoint: 960,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    // variableWidth: false,
+                    adaptiveHeight: true,
 
-    })
+                }
+            },
+            {
+                breakpoint: 780,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 2
+                }
+            },
+            // {
+            //     breakpoint: 480,
+            //     settings: {
+            //         slidesToShow: 1,
+            //         slidesToScroll: 1
+            //     }
+            // }
+        ]
+    });
+
+    $slickSlider.find(".slick-slide").height("auto");
+    $slickSlider.slickSetOption(null, null, true);
+
+    $('.nav__btn-menu').on('click', function () {
+        $('.nav__list').slideToggle();
+    });
 
 });
