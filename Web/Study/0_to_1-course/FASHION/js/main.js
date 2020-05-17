4c70a2c2ea4080b0801a5127238e6f1d44cc1410
Alexander Grobovsky <ktotaam@mail.ru>
2020-05-18 02:09:55 +0500

    done Fashion (started adaptive)

---
+++ Web/Study/0_to_1-course/FASHION/js/main.js
@@ -0,0 +1,39 @@
+$(function () {
+
+    $('.continue-btn').on('mouseover', function () {
+        $(this).parent().css({'box-shadow' : 'none'})
+    })
+    .on('mouseleave', function () {
+        $(this).parent().css({'box-shadow' : '0 2px 6px rgba(0, 0, 0, 0.1)'})
+
+
+    });
+
+    var mySwiper= new Swiper('.swiper1', {
+        direction: 'horizontal',
+        loop: true,
+        effect: 'slide',
+        keyboard: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+    });
+
+    var mySwiper= new Swiper('.swiper2', {
+        slidesPerView: 6,
+        keyboard: true,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+
+    });
+
+});
