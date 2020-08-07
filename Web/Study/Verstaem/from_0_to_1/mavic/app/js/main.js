0675eecb14ff66ceb61e4e8a4be4e9d626e7905e
Alexander Grobovsky <ktotaam@mail.ru>
2020-08-07 12:48:50 +0500

    save

--- Web/Study/Verstaem/from_0_to_1/mavic/app/js/main.js
+++ Web/Study/Verstaem/from_0_to_1/mavic/app/js/main.js
@@ -1,3 +1,13 @@
 $(function () {
-
+    $('.products__slider').slick({
+        prevArrow: '<button class="products__slider-arrow arrow-left">' +
+            '<svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">' +
+            '<path d="M9.21839 1L1 9L9.21839 17" stroke="white"/>' +
+            '</svg></button>',
+        nextArrow: '<button class="products__slider-arrow arrow-right">' +
+            '<svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">' +
+            '<path d="M0.78161 17L9 9L0.78161 1" stroke="black"/>' +
+            '</svg></button>',
+        infinite: false,
+    })
 });
