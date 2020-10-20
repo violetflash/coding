24a1c94daff347e0800dcb45047990fd009cd19f
Alex Grobovsky <ktotaam@mail.ru>
2020-10-20 23:34:37 +0500

    save

--- Web/Projects/Zonex-max-graph/README.md
+++
@@ -1,2 +0,0 @@
-# gulp-maxgraph
-My gulp settings
--- Web/Projects/start-new-project/#src/js/app.js
+++
@@ -1,4 +0,0 @@
-@@include('files/sliders.js', {})
-@@include('files/functions.js', {})
-@@include('files/script.js', {})
-@@include('files/dynamic-adapt.js', {})
