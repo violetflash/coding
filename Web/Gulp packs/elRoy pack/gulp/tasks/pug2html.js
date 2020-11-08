5ffde93d4b92cb0f6ae206b5a3248590aac00228
Alex Grobovsky <ktotaam@mail.ru>
2020-11-09 00:55:19 +0500

    save

--- Web/Gulp packs/elRoy pack/gulp/tasks/pug2html.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp')
-const pug = require('gulp-pug')
-
-module.exports = function pug2html(cb) {
-  return gulp.src('src/pages/*.pug')
-      .pipe(pug())
-      .pipe(gulp.dest('build'))
-}
